985c490b71261903355437b0e3ac3f2f39a2fedc
Alex Ramsdell <alexander.ramsdell@ns8.com>
2020-01-02 23:01:26 -0500

    cleanup

--- src/web/index.js
+++ src/web/index.js
@@ -4,13 +4,14 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const { PATHS, SERVER } = require('../config')
 const routes = require('./routes')
 const Logger = require('../lib/logger')
 
 const logger = new Logger({ logDir: PATHS.LOGS })
-
 const app = express()
+
 app.engine('hbs', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
@@ -29,18 +30,14 @@ app.use((req, res, next) => {
 // bind routes
 app.get('/', routes.index)
 app.post('/authorize', routes.authorize)
-app.get('/store_subject_data', routes.addParticipant)
-/*
-app.post('/quit', routes.stopServer)
-*/
+app.get('/store_subject_data', routes.addParticipant) // leave url name for now, set in fb dev panel
 
 function start() {
 
   app.listen(SERVER.PORT, () => {
 
-    // logger
-    console.log(`Fbtrack Registration Server running at http://localhost:${SERVER.PORT}...`)
     const localUrl = `http://localhost:${SERVER.PORT}`
+    logger.info(`Fbtrack Registration Server running at ${ localUrl }...`)
 
     exec(
         `open -a '/Applications/Google Chrome.app' ${ localUrl }`,
