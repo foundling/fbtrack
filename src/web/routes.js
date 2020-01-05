2c229c7994b5d520dca164ed85f4b855948861ce
Alex Ramsdell <alexander.ramsdell@ns8.com>
2020-01-04 20:12:32 -0500

    add more info in signup success page, clean up routes.

--- src/web/routes.js
+++ src/web/routes.js
@@ -1,5 +1,5 @@
 const qs = require('querystring')
-const { 
+const {
   OAUTH,
   PATHS,
   SERVER,
@@ -23,16 +23,16 @@ const logger = new Logger({
   }
 })
 
-const { 
+const {
 
-  logToUserSuccess, 
+  logToUserSuccess,
   logToUserFail,
   ymdFormat
 
-} = require('../lib/utils') 
+} = require('../lib/utils')
 
 const index = (req, res) => {
-  res.render('index', { 
+  res.render('index', {
     layout: 'main.hbs',
     studyName: STUDY_NAME
   })
@@ -40,41 +40,48 @@ const index = (req, res) => {
 
 const authorize = async (req, res) => {
 
-  const { participantId } = req.query 
+  const { participantId } = req.query
+  let participant
 
-  if (!participantId)
+  if (!participantId) {
     return res.status(400).send({ errorMessage: 'Bad request. No participant id.' })
+  }
 
   try {
 
-    const participant = await db.getParticipantByParticipantId(participantId)
-
-    if (participant) {
-      return res.status(409).send({ errorMessage: 'This participant id already exists in the database.' })
-    } else {
-
-      try {
-        // let client do the redirect to server. if redirecting via ajax call, it's a cross-domain request,
-        // but blocked by fitbit.
-        
-        const state = qs.encode({ participantId })
-        const prompt = 'login consent'
-        const redirectURI = client.getAuthorizeUrl(OAUTH.SCOPE, OAUTH.CALLBACK_URL, prompt, state) 
-        await res.json({ data: { redirectURI } })
-      } catch(e) {
-        throw e
-        await res.json({ errorMessage: e })
-      }
-    }
+    participant = await db.getParticipantById(participantId)
 
   } catch(e) {
 
-      throw e;
-      return res.setStatus(500).end()
+    throw e;
+    return res.setStatus(500).end()
 
   }
 
-};
+  if (participant) {
+
+    return res.status(409).send({ errorMessage: 'This participant id already exists in the database.' })
+
+  }
+
+  try {
+
+    /* let front-end client perform redirect to fibit. if redirecting via ajax response,
+       it's a cross-domain request, which fitbit blocks. */
+
+    const state = qs.encode({ participantId })
+    const prompt = 'login consent'
+    const redirectURI = client.getAuthorizeUrl(OAUTH.SCOPE, OAUTH.CALLBACK_URL, prompt, state)
+
+    await res.json({ data: { redirectURI } })
+
+  } catch(e) {
+
+    throw e
+    await res.json({ errorMessage: e })
+
+  }
+}
 
 async function addParticipant(req, res) {
 
@@ -84,29 +91,34 @@ async function addParticipant(req, res) {
   const todaysDate = format(new Date(), ymdFormat);
 
   if (error) {
+
     logger.error(`Error receiving auth tokens`)
     return res.render('signup_status', { layout: 'main.hbs', error: e })
+
   }
 
   let tokens
 
   try {
 
-    tokens = { access_token, refresh_token } = await client.getAccessToken(code, OAUTH.CALLBACK_URL)
+    tokens = {
+      access_token,
+      refresh_token
+    } = await client.getAccessToken(code, OAUTH.CALLBACK_URL)
 
   } catch(e) {
 
     logger.error(`Error getting access tokens: ${e}`)
-    return res.render('signup_status', { 
+    return res.render('signup_status', {
       layout: 'main.hbs',
-      error: e 
+      error: e
     })
 
   }
-  const newParticipantData = { 
+  const newParticipantData = {
     participantId,
     registrationDate: todaysDate,
-    accessToken: tokens.access_token, 
+    accessToken: tokens.access_token,
     refreshToken: tokens.refresh_token,
     isActive: 1,
   }
@@ -114,12 +126,18 @@ async function addParticipant(req, res) {
   try {
 
     await db.addParticipant(newParticipantData)
-    return res.render('signup_status', { layout: 'main.hbs' })
+    return res.render('signup_status', {
+      layout: 'main.hbs',
+      participantId
+    })
 
   } catch (e) {
 
     logger.error(e)
-    return res.render('signup_status', { layout: 'main.hbs', error: e })
+    return res.render('signup_status', {
+      error: e,
+      layout: 'main.hbs',
+    })
 
   }
 
@@ -127,18 +145,16 @@ async function addParticipant(req, res) {
 
 const stopServer = (req, res) => {
 
-    logger(`stopped server session for subject with id: ${ db.sessionCache.get('subjectId') }`, () => {
-        console.log('Stopping the local server ...');
-        process.exit(0);
-    });
+  logger.info('Stopping server ...')
+  process.exit()
 
 };
 
 module.exports = exports = {
 
-    index,
-    authorize,
-    addParticipant,
-    stopServer
+  index,
+  authorize,
+  addParticipant,
+  stopServer
 
 };
