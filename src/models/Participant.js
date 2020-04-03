2084c787897be7c4c83859e955fc5717519a908a
Alex Ramsdell <alexramsdell@gmail.com>
2020-04-03 17:20:21 -0400

    Convert object memos to maps to make general tabulation of metrics collected/missing datastructure standard. signature: [ date string => [ metric name => query path ] ].
    Also, refactor refresh access token code into separate function.

--- src/models/Participant.js
+++ src/models/Participant.js
@@ -17,7 +17,7 @@ const { WINDOW_SIZE } = USER_CONFIG
 const {
   CLIENT_ID,
   CLIENT_SECRET,
-  ENDPOINTS
+  ENDPOINTS,
 } = FITBIT_CONFIG
 
 const {
@@ -31,7 +31,6 @@ const Database = require(DB_PATH)
 
 const {
   datesFromRange,
-  dateRangeFromDateStrings,
   dateRE,
   ymdFormat,
 } = dates
@@ -61,19 +60,203 @@ const fbClient = new FitbitClient({
   clientSecret: CLIENT_SECRET
 })
 
-function generateQueryPathsByDate({ metricsByDate, endpoints }) {
+class Participant {
 
-  const memo = {}
+  constructor({ files, participantId, record }) {
 
-  for (const dateString in metricsByDate) {
+    this.files = files
+    this.participantId = record.participantId
+    this.record = record
 
-    if (!memo[dateString]) {
-      memo[dateString] = {}
+  }
+ 
+  async buildQueryPathsByDate(start, stop) {
+
+    const expectedDateStrings = datesFromRange({ start, stop }).map(d => format(d, ymdFormat))
+
+    const filenames = await getFiles({
+      directory: RAW_DATA_PATH,
+      criterion: fname => fname.startsWith(this.participantId) &&
+                          expectedDateStrings.some(s => fname.includes(s)),
+    })
+
+    const missingMetricsByDate = await findUncapturedDates({
+      filenames,
+      expectedDateStrings,
+      metrics: [...ENDPOINTS.keys()],
+    })
+
+    const noMissingDataForDates = [...missingMetricsByDate.values()].every(metricsForDate => metricsForDate.size === 0)
+
+    if (noMissingDataForDates) {
+      await logger.info(`All dates captured for participant ${this.participantId} in this date range.`)
+      return new Map()
     }
 
-    for (const metric in metricsByDate[dateString]) {
+    return generateQueryPathsByDate({
+      metricsByDate: missingMetricsByDate,
+      endpoints: ENDPOINTS,
+    })
 
-      memo[dateString][metric] = endpoints[metric].replace('%DATE%', dateString)
+  }
+
+  async query(start, stop) {
+
+    const queryPathsByDate = await this.buildQueryPathsByDate(start, stop)
+
+    for (const [date, paths] of queryPathsByDate) {
+
+      console.log(`DATE: ${date}`)
+
+      if (paths.size === 0) {
+        logger.info(`All metrics for participant ${this.participantId} have been captured for dates in range.`)
+        continue
+      }
+
+      for (const [metric, queryPath] of paths) {
+
+        const metricData = await this.queryFitbit(queryPath)
+        const filename = this.buildFilename({ participantId: this.participantId, date, metric, extension: 'json' })
+        const outputPath = path.join(RAW_DATA_PATH, filename)
+
+        await writeFilePromise(outputPath, JSON.stringify(metricData))
+
+        console.log(`${metric} ✓`)
+
+      }
+
+    }
+
+  }
+
+  async queryFitbit(queryPath) {
+
+    let body
+    let response
+
+    try {
+
+      [ body, response ] = await fbClient.get(queryPath, this.record.accessToken)
+
+    } catch(e) {
+
+      logger.error(`attempt to query Fitbit failed:\n${e}`)
+      throw e
+
+    }
+
+    if (isSuccess(response)) {
+
+      return body
+
+    }
+
+    if (accessTokenExpired(response)) {
+
+      await this.refreshAccessToken()
+
+      try {
+
+        const [ retryBody, retryResponse ] = await fbClient.get(queryPath, this.record.accessToken)
+        return retryBody
+
+      } catch(e) {
+
+        logger.error(`queryFitbit - query after refreshAccessToken has failed: ${e}`)
+        throw e
+
+      }
+
+    }
+
+    if (rateLimitExceeded(response)) {
+
+      const leeway = 60
+      const secondsToWait = parseInt(response.headers['retry-after']) + leeway
+      const resumeTime = format(addSeconds(new Date(), secondsToWait), 'hh:mm')
+
+      logger.error(`queryFitbit - rate limit exceeded. Waiting ${secondsToWait} seconds to resume`)
+      logger.error(`Starting againt at ${resumeTime}.`)
+
+      await utils.sleep(secondsToWait + leeway)
+
+      try {
+
+        const [ retryBody, retryResponse ] = await fbClient.get(queryPath, this.record.accessToken)
+        return retryBody
+
+      } catch(e) {
+
+        logger.error(`queryFitbit - retry after rateLimitExceeded has failed: ${e}`)
+        throw e
+
+      }
+
+    }
+
+    if (invalidRefreshToken(response)) {
+
+      // warning: this is bad news, and annoying to deal with.
+      // make sure database never can't accept bad data for an auth token.
+      logger.error(`queryFitbit - InvalidRefreshToken Error for participant ${this.participantId}.`)
+      logger.error('The participant needs to be re-authorized with the application.')
+      logger.error(`skipping participant ${this.participantId}.`)
+
+      return
+
+    }
+
+  }
+
+  async refreshAccessToken() {
+
+    try {
+
+      const tokenExpiresIn = 3600
+      const { access_token, refresh_token } = await fbClient.refreshAccessToken(accessToken, refreshToken, tokenExpiresIn)
+
+      await db.updateAccessTokensById({
+        participantId: this.participantId,
+        accessToken: access_token,
+        refreshToken: refresh_token,
+      })
+
+      this.record = await db.getParticipantById(this.participantId)
+
+    } catch(e) {
+
+      if (e.status === 400 && e.context.errors[0].errorType === 'invalid_grant') {
+        logger.error(`Failed to refresh participant ${this.participantId}'s Refresh Access Token.`)
+        return
+      }
+
+    }
+  }
+
+  buildFilename({ participantId, date, metric, extension='json'}) {
+
+    return `${participantId}_${date}_${metric}.json`
+
+  }
+
+}
+
+function generateQueryPathsByDate({ metricsByDate, endpoints }) {
+
+  // 'metricToTemplatePathMap' is an example of where an explicit type system could
+  // liberate you from the ugly naming
+  const memo = new Map()
+
+  for (const [dateString, metricToTemplatePathMap] of metricsByDate) {
+
+    if (!memo.has(dateString)) {
+      memo.set(dateString, new Map())
+    }
+
+    for (const [metric, templatePathMap] of metricToTemplatePathMap) {
+
+      const populatedTemplate = endpoints.get(metric).replace('%DATE%', dateString)
+      memo.get(dateString).set(metric, populatedTemplate)
 
     }
   }
@@ -82,171 +265,25 @@ function generateQueryPathsByDate({ metricsByDate, endpoints }) {
 
 }
 
-async function queryFitbit(queryPath) {
-
-  const [ body, response ] = await fbClient.get(queryPath, participant.accessToken)
-
-  if (isSuccess(response)) {
-    return body
-  }
-
-  if (accessTokenExpired(response)) {
-
-    try {
-      const { access_token:accessToken, refresh_token:refreshToken } = await refreshAccessToken({
-        participantId: participant.participantId,
-        accessToken: participant.accessToken,
-        refreshToken: participant.refreshToken
-      })
-    } catch(e) {
-      if (e.status === 400 && e.context.errors[0].errorType === 'invalid_grant') {
-        logger.error(`Failed to refresh participant ${this.participantId}'s Refresh Access Token.`)
-        return
-      }
-    }
-
-    // warning: if this fails, you will have to re-auth the subject. important!
-    await db.updateAccessTokensById({
-      participantId: participant.participantId,
-      accessToken,
-      refreshToken
-    })
-
-    const [ retryBody, retryResponse ] = await fbClient.get(queryPath, accessToken)
-    return retryBody
-
-  }
-
-  if (rateLimitExceeded(response)) {
-
-    const leeway = 60
-    const secondsToWait = parseInt(response.headers['retry-after']) + leeway
-    const resumeTime = format(addSeconds(new Date(), secondsToWait), 'hh:mm')
-
-    logger.error(`Rate limit exceeded. Waiting ${secondsToWait} seconds to resume`)
-    logger.error(`Starting againt at ${resumeTime}.`)
-
-    await utils.sleep(secondsToWait + leeway)
-
-    const [ retryBody, retryResponse ] = await fbClient.get(queryPath, participant.accessToken)
-
-    return retryBody
-
-  }
-
-  if (invalidRefreshToken(response)) {
-
-    // warning: this is bad news, and annoying to deal with. 
-    // make sure database never can't accept bad data for an auth token.
-    logger.error(`InvalidRefreshToken Error for participant ${participantId}.`)
-    logger.error('The participant needs to be re-authorized with the application.')
-    logger.error(`skipping participant ${participantId}.`) 
-
-    return
-
-  }
-
-}
-
 async function findUncapturedDates({ filenames, expectedDateStrings, metrics }) {
 
-  const missing = {}
+  const missing = new Map()
+
   for (const dateString of expectedDateStrings) {
-    missing[dateString] = metrics.reduce((o, metric) => {
-      o[metric] = true;
-      return o
-    }, {}) 
+    missing.set(dateString, new Map())
+    for (const metric of metrics) {
+      missing.get(dateString).set(metric, true)
+    }
   }
+
 
   for (const filename of filenames) {
     const [ id, dateString, metric, extension ] = filename.split(/[._]/)
-    delete missing[dateString][metric]
+    missing.get(dateString).delete(metric)
   }
 
   return missing
 
 }
 
-async function refreshAccessToken({ accessToken, refreshToken, participantId }) {
-
-  const expirationWindow = 3600
-
-  return await fbClient.refreshAccessToken(accessToken, refreshToken, expirationWindow)
-
-}
-
-class Participant {
-
-  constructor({ participantId, files, record }) {
-
-    this.files = files
-    this.participantId = record.participantId
-    this.record = record
-
-  }
-  
-  async buildQueryPathsByDate(start, stop) {
-
-    const expectedDateStrings = datesFromRange({ start, stop }).map(d => format(d, ymdFormat))
-
-    const filenames = await getFiles({
-      directory: RAW_DATA_PATH,
-      criterion: fname => fname.startsWith(this.participantId) && expectedDateStrings.some(s => fname.includes(s)),
-    })
-
-    const missingMetricsByDate = await findUncapturedDates({
-      filenames,
-      expectedDateStrings,
-      metrics: Object.keys(ENDPOINTS),
-    })
-
-    const allDataCaptured = Object.entries(missingMetricsByDate)
-      .every(metricsByDate => Object.entries(metricsByDate).length === 0)
-
-    if (allDataCaptured) {
-      await logger.info(`All dates captured for participant ${participantId} in this date range.`) 
-      return
-    }
-
-    return generateQueryPathsByDate({
-      metricsByDate: missingMetricsByDate,
-      endpoints: ENDPOINTS
-    })
-
-  }
-
-  buildMetricFilename({ participantId, date, metric, extension='json'}) {
-    return `${participantId}_${date}_${metric}.json` 
-  }
-
-  async query(start, stop) {
-
-    const objectIsEmpty = o => Object.entries(o).length === 0
-    const queryPathsByDate = await this.buildQueryPathsByDate(start, stop)
-
-    for (const [date, paths] of queryPathsByDate.entries()) {
-
-      console.log(`DATE: ${date}`)
-
-      if (objectIsEmpty(paths)) {
-        logger.info(`All data for participant ${this.participantId} in window has been retrieved.`)
-        continue
-      }
-
-      for (const [metric, path] of paths.entries()) {
-        const metricData = await queryFitbit(path)
-        const outputPath = path.join(
-          RAW_DATA_PATH,
-          this.buildMetricFilename({ participantId, date, metric, extension: 'json' })
-        )
-        await writeFilePromise(outputPath, JSON.stringify(metricData))
-        console.log(`${metric} ✓`)
-      }
-
-    }
-
-  }
-
-}
-
-module.exports = Participant
+module.exports = exports = Participant
